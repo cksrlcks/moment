3added61f4e33edf378b7839a44b3385f125337b
chanki <chanki.kim89@gmail.com>
2023-12-22 15:19:41 +0900

    로고추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Dockbar from "./_component/dockbar";
+import Image from "next/image";
+import logo from "../../public/logo.svg";
 
 export const metadata: Metadata = {
     title: "At the Moment",
@@ -12,7 +14,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="ko">
             <body>
                 <div id="app">
-                    <div className="app-inner">{children}</div>
+                    <div className="app-inner">
+                        <header className="app-header">
+                            <Image src={logo} className="logo" alt="at the moment" />
+                        </header>
+                        <div className="app-body">{children}</div>
+                    </div>
                     <Dockbar />
                 </div>
             </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ export default function Home() {
     return (
         <div>
             {/* <MomentList /> */}
+            <div className="temp">
             저장된 카드리스트들 올곳
+            </div>
         </div>
     );
 }
